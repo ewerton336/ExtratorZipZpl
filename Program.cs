75d20fd1246d497026e217ee9579c29a3f0af818
Ewerton Guimarães <ewerton.guimaraes@becomex.com.br>
2023-05-30 11:00:54 -0300

    correção de diretórios

--- Program.cs
+++ Program.cs
@@ -1,76 +1,99 @@
-﻿using System.IO.Compression;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
 using Microsoft.Extensions.Configuration;
 
-
-
-// Carrega as configurações do arquivo appsettings.json
-IConfigurationRoot configuration = new ConfigurationBuilder()
+namespace FileExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Carrega as configurações do arquivo appsettings.json
+            IConfiguration configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json")
                 .Build();
 
-// Obtém o caminho do diretório a ser consultado
-string directoryIn = configuration.GetSection("DirectoryIn").Value;
+            // Obtém o caminho do diretório de entrada e saída
+            string directoryIn = configuration.GetSection("DirectoryIn").Value;
+            string directoryOut = configuration.GetSection("DirectoryOut").Value;
 
-if (string.IsNullOrEmpty(directoryIn))
-{
-    Console.WriteLine("Diretório de entrada inválido. Verifique as configurações em appsettings.json.");
-    return;
-}
+            if (string.IsNullOrEmpty(directoryIn) || string.IsNullOrEmpty(directoryOut))
+            {
+                Console.WriteLine("Diretórios inválidos. Verifique as configurações em appsettings.json.");
+                return;
+            }
 
-// Cria o diretório "extraídos" dentro do diretório de saída
-string directoryOut = configuration.GetSection("DirectoryOut").Value;
-string extractedFolderPath = Path.Combine(directoryOut, "extraídos");
-Directory.CreateDirectory(extractedFolderPath);
+            // Cria o diretório "extraídos" dentro do diretório de saída
+            string extractedFolderPath = Path.Combine(directoryOut, "extraídos");
+            Directory.CreateDirectory(extractedFolderPath);
 
-// Obtém todos os arquivos no diretório de entrada com extensão .zip e contendo a palavra "ZPL" no nome
-IEnumerable<string> files = Directory.EnumerateFiles(directoryIn, "*.zip", SearchOption.AllDirectories)
-    .Where(file => Path.GetFileName(file).IndexOf("ZPL", StringComparison.OrdinalIgnoreCase) >= 0);
+            // Obtém todos os arquivos no diretório de entrada com extensão .zip e contendo a palavra "ZPL" no nome
+            IEnumerable<string> files = Directory.EnumerateFiles(directoryIn, "*.zip", SearchOption.AllDirectories)
+                .Where(file => Path.GetFileName(file).IndexOf("ZPL", StringComparison.OrdinalIgnoreCase) >= 0);
 
-foreach (string file in files)
-{
-    Console.WriteLine($"Extraindo arquivo: {file}");
+            foreach (string file in files)
+            {
+                Console.WriteLine($"Extraindo arquivo: {file}");
 
-    try
-    {
-        // Extrai o arquivo para o diretório de saída
-        ZipFile.ExtractToDirectory(file, extractedFolderPath);
-        Console.WriteLine("Extração concluída com sucesso.");
+                try
+                {
+                    // Extrai o arquivo para o diretório temporário
+                    string tempFolderPath = Path.Combine(directoryOut, "temp");
+                    ZipFile.ExtractToDirectory(file, tempFolderPath);
+                    Console.WriteLine("Extração concluída com sucesso.");
 
-        // Move o arquivo extraído para a pasta "extraídos"
-        string extractedFileName = Path.GetFileName(file);
-        string extractedFilePath = Path.Combine(extractedFolderPath, extractedFileName);
-        string destinationFilePath = GetUniqueFilePath(extractedFolderPath, extractedFileName);
-        File.Move(extractedFilePath, destinationFilePath);
-        Console.WriteLine($"Arquivo movido para a pasta 'extraídos' com o nome '{Path.GetFileName(destinationFilePath)}'.");
+                    // Move o arquivo extraído para o diretório de saída
+                    string extractedFileName = Path.GetFileName(file);
+                    string[] extractedFiles = Directory.GetFiles(tempFolderPath, "*", SearchOption.AllDirectories);
 
-        // Move o arquivo original para a pasta "extraídos"
-        string originalDestinationFilePath = Path.Combine(extractedFolderPath, extractedFileName);
-        File.Move(file, originalDestinationFilePath);
-        Console.WriteLine($"Arquivo original movido para a pasta 'extraídos' com o nome '{Path.GetFileName(originalDestinationFilePath)}'.");
+                    foreach (string extractedFile in extractedFiles)
+                    {
+                        string relativePath = Path.GetRelativePath(tempFolderPath, extractedFile);
+                        string destinationFilePath = Path.Combine(directoryOut, relativePath);
+
+                        // Verifica se o arquivo já existe no diretório de saída
+                        if (File.Exists(destinationFilePath))
+                        {
+                            string fileName = Path.GetFileNameWithoutExtension(destinationFilePath);
+                            string fileExtension = Path.GetExtension(destinationFilePath);
+                            string newFileName = GetUniqueFileName(fileName, fileExtension);
+                            string newFilePath = Path.Combine(directoryOut, newFileName);
+                            File.Move(extractedFile, newFilePath);
+                            Console.WriteLine($"Arquivo movido para o diretório de saída com o nome '{newFileName}'.");
+                        }
+                        else
+                        {
+                            File.Move(extractedFile, destinationFilePath);
+                            Console.WriteLine($"Arquivo movido para o diretório de saída com o nome '{Path.GetFileName(destinationFilePath)}'.");
+                        }
+                    }
+
+                    // Move o arquivo .zip para a pasta "extraídos"
+                    string zipDestinationFilePath = Path.Combine(extractedFolderPath, extractedFileName);
+                    File.Move(file, zipDestinationFilePath);
+                    Console.WriteLine($"Arquivo ZIP movido para a pasta 'extraídos' com o nome '{Path.GetFileName(zipDestinationFilePath)}'.");
+
+                    // Exclui o diretório temporário
+                    Directory.Delete(tempFolderPath, true);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erro ao extrair arquivo: {ex.Message}");
+                }
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para sair.");
+            Console.ReadKey();
+        }
+
+        static string GetUniqueFileName(string fileName, string fileExtension)
+        {
+            string newFileName = $"{fileName} ({Guid.NewGuid()}){fileExtension}";
+            return newFileName;
+        }
     }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Erro ao extrair arquivo: {ex.Message}");
-    }
-}
-
-Console.WriteLine("Pressione qualquer tecla para sair.");
-Console.ReadKey();
-
-string GetUniqueFilePath(string directory, string fileName)
-{
-    string filePath = Path.Combine(directory, fileName);
-    string fileExtension = Path.GetExtension(fileName);
-    string fileNameOnly = Path.GetFileNameWithoutExtension(fileName);
-    int counter = 1;
-
-    while (File.Exists(filePath))
-    {
-        string newFileName = $"{fileNameOnly} ({counter}){fileExtension}";
-        filePath = Path.Combine(directory, newFileName);
-        counter++;
-    }
-
-    return filePath;
 }
